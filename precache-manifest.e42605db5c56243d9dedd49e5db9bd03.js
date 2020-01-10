a6852c682573e1b30bfa5ad389b5964a282acfef
Hugo Georget <hugo.georget@sword-group.com>
2020-01-10 11:58:04 +0100

    Updates

---
+++ precache-manifest.e42605db5c56243d9dedd49e5db9bd03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a226c0d1ea8fcbcf1728b6f23da9b4",
+    "url": "/fcc-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "f903e550a6e77963b973",
+    "url": "/fcc-pomodoro-clock/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "0a1267b8b0f21eac6d82",
+    "url": "/fcc-pomodoro-clock/static/js/2.cce32c76.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fcc-pomodoro-clock/static/js/2.cce32c76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f903e550a6e77963b973",
+    "url": "/fcc-pomodoro-clock/static/js/main.bf7b095c.chunk.js"
+  },
+  {
+    "revision": "5172afddc5054385b73b",
+    "url": "/fcc-pomodoro-clock/static/js/runtime-main.e9628f55.js"
+  }
+]);
